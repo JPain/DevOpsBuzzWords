d19526d2c8da75f9f6348561ca815088c09b26a0
James Pain <jpain@veinteractive.com>
2016-04-19 10:15:05 +0100

    Init

--- DevOpsBuzzWords/Controllers/ValuesController.cs
+++ DevOpsBuzzWords/Controllers/ValuesController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Swashbuckle.Swagger.Annotations;
+using DevOpsBuzzWords.Models;
 
 namespace DevOpsBuzzWords.Controllers
 {
@@ -12,41 +13,52 @@ namespace DevOpsBuzzWords.Controllers
     {
         // GET api/values
         [SwaggerOperation("GetAll")]
-        public IEnumerable<string> Get()
+        public SlackResponse Get()
         {
-            return new string[] { "value1", "value2" };
+            var words = new string[] { "We need more Big data", "Containers!!!", "Scale more", "We need Innovation", "What about Quantum Computing?", "Agile it", "Lean Enterprise", "Innovate", "You only fail when you fail to learn", "Do the right thing it is as important as doing the thing right", "Microservices!", "Woman in code", "What is devops?" };
+            var rand = new Random();
+
+            var word = words[rand.Next(0, words.Length)];
+
+            var result = new SlackResponse()
+            {
+                response_type = "in_channel",
+                text = word
+            };
+
+            return result;
         }
 
-        // GET api/values/5
-        [SwaggerOperation("GetById")]
-        [SwaggerResponse(HttpStatusCode.OK)]
-        [SwaggerResponse(HttpStatusCode.NotFound)]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //// GET api/values/5
+        //[SwaggerOperation("GetById")]
+        //[SwaggerResponse(HttpStatusCode.OK)]
+        //[SwaggerResponse(HttpStatusCode.NotFound)]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
-        // POST api/values
-        [SwaggerOperation("Create")]
-        [SwaggerResponse(HttpStatusCode.Created)]
-        public void Post([FromBody]string value)
-        {
-        }
+        //// POST api/values
+        //[SwaggerOperation("Create")]
+        //[SwaggerResponse(HttpStatusCode.Created)]
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
-        // PUT api/values/5
-        [SwaggerOperation("Update")]
-        [SwaggerResponse(HttpStatusCode.OK)]
-        [SwaggerResponse(HttpStatusCode.NotFound)]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT api/values/5
+        //[SwaggerOperation("Update")]
+        //[SwaggerResponse(HttpStatusCode.OK)]
+        //[SwaggerResponse(HttpStatusCode.NotFound)]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE api/values/5
-        [SwaggerOperation("Delete")]
-        [SwaggerResponse(HttpStatusCode.OK)]
-        [SwaggerResponse(HttpStatusCode.NotFound)]
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/values/5
+        //[SwaggerOperation("Delete")]
+        //[SwaggerResponse(HttpStatusCode.OK)]
+        //[SwaggerResponse(HttpStatusCode.NotFound)]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
---
+++ DevOpsBuzzWords/Models/SlackResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevOpsBuzzWords.Models
+{
+    public class SlackResponse
+    {
+        public string response_type { get; set; }
+        public string text { get; set; }
+        
+    }
+}
